23bfc3a79db2cfa198aa88924fc2ccad0fb95185
Rezan-Soylemez <rezan.sm@gmail.com>
2023-07-01 03:36:55 +0300

    Featured - SeriLog Configurations

--- srv/ChatServer/Hubs/ChatHub.cs
+++ srv/ChatServer/Hubs/ChatHub.cs
@@ -4,6 +4,6 @@ public class ChatHub : Hub
 {
     public async Task SendMessage(string user, string message)
     {
-        await Clients.All.SendAsync("ReceiveMessage", user, message);
+        await Clients.All.SendAsync("ReceiveMessage", user, message);    
     }
 }
--- srv/ChatServer/Program.cs
+++ srv/ChatServer/Program.cs
@@ -1,6 +1,15 @@
+using Serilog;
+using Serilog.Events;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+Log.Logger = new LoggerConfiguration()
+       .MinimumLevel.Information()
+       .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+       .Enrich.FromLogContext()
+       .WriteTo.Console()
+       .CreateLogger();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -9,6 +18,12 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddSignalR();
 builder.Services.AddControllers();
 
+// SeriLog Configuration
+builder.Services.AddLogging(loggingBuilder =>
+{
+    loggingBuilder.ClearProviders();
+    loggingBuilder.AddSerilog(dispose: true);
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
