1534d5ac70518520880fa9a145b8e26e0170079a
Rezan-Soylemez <rezan.sm@gmail.com>
2023-07-02 23:24:08 +0300

    Featured - Log And User Models Are Added

---
+++ srv/ChatServer/Models/Log.cs
@@ -0,0 +1,22 @@
+﻿namespace ChatServer.Models;
+
+public class Log:Entity
+{
+    public  int MessageId { get; set; }
+    public int UserId { get; set; }
+
+
+    public User User{ get; set; }
+    public Message Message { get; set; }
+
+    public Log()
+    {
+    }
+    public Log(int id ,int messageId, int userId, User user):this()
+    {
+        Id = id; 
+        MessageId = messageId;
+        UserId = userId;
+        User = user;
+    }
+}
---
+++ srv/ChatServer/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace ChatServer.Models;
+public class User:Entity
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public User()
+    {
+            
+    }
+    public User(int id ,string firstName, string lastName):this()
+    {   
+        Id = id ;
+        FirstName = firstName;
+        LastName = lastName;
+    }
+}
